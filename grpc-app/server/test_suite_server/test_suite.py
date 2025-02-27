62fdb6c2d4750bd8f4a6fdf124fd6dd8c138b298
DRIES ROORYCK <dries_rooryck@college.harvard.edu>
2025-02-27 00:01:24 -0500

    renumbering tests
    ^^

--- grpc-app/server/test_suite_server/test_suite.py
+++ grpc-app/server/test_suite_server/test_suite.py
@@ -9,10 +9,8 @@ from test_6_test_invalid_login import TestInvalidLogin
 from test_7_auth_required import TestAuthenticationRequired
 from test_8_logout_login import TestLogoutLogin
 from test_9_send_messsages_to_client import TestSendMessagesToClient
-from test_10_delete_single_message import TestDeleteSingleMessage
-from test_11_delete_multiple_messages import TestDeleteMultipleMessages
-from test_12_delete_account import TestDeleteAccount
-from test_13_list_accounts import TestListAccounts
+from server.test_suite_server.test_10_delete_account import TestDeleteAccount
+from server.test_suite_server.test_11_list_accounts import TestListAccounts
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(
@@ -26,8 +24,6 @@ if __name__ == "__main__":
             unittest.defaultTestLoader.loadTestsFromTestCase(TestAuthenticationRequired),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestLogoutLogin),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestSendMessagesToClient),
-            unittest.defaultTestLoader.loadTestsFromTestCase(TestDeleteSingleMessage),
-            unittest.defaultTestLoader.loadTestsFromTestCase(TestDeleteMultipleMessages),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestDeleteAccount),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestListAccounts)
         ])
